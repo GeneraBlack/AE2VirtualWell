2ba68e6ac96b5d77aefd315b66a80d1b60de4eef
GeneraBlack <meier.Alexander1@googlemail.com>
2026-09-18 17:48:35 +0200

    Use OnDatapackSyncEvent for drop cache invalidation on datapack sync

--- src/main/java/de/project/ae2virtualwell/AE2VirtualWell.java
+++ src/main/java/de/project/ae2virtualwell/AE2VirtualWell.java
@@ -41,7 +41,7 @@ public class AE2VirtualWell {
         modEventBus.addListener(this::commonSetup);
 
         // Clear drop cache on server start or datapack reload so custom recipes update immediately
-        net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener((net.neoforged.neoforge.event.AddReloadListenerEvent event) -> de.project.ae2virtualwell.recipe.WellDropRegistry.clearCache());
+        net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener((net.neoforged.neoforge.event.OnDatapackSyncEvent event) -> de.project.ae2virtualwell.recipe.WellDropRegistry.clearCache());
         net.neoforged.neoforge.common.NeoForge.EVENT_BUS.addListener((net.neoforged.neoforge.event.server.ServerStartedEvent event) -> de.project.ae2virtualwell.recipe.WellDropRegistry.clearCache());
     }
 
